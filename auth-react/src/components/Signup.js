8a73747f328dcb5eb66fbd21fd9e8e5c4ba9898a
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-04-28 21:13:29 +0530

    fix - fixed the signup component issues

--- auth-react/src/components/Signup.js
+++ auth-react/src/components/Signup.js
@@ -1,42 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Form, Button, Card } from "react-bootstrap";
 
 const Signup = (props) => {
-  return;
-  <>
-    <Card.Body>
-    <h2 className="text-center mb-4"> Signup </h2>
-     
-       <Form>
-         <Form.Group id = "email">
-         <Form.Label>Email</Form.Label>
-         <Form.Control type = "email" ref = {emailref} required />
-         </Form.Group>
+  const emailref = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  return (
+    <>
+      <Card.Body>
+        <h2 className="text-center mb-4"> Signup </h2>
 
+        <Form>
+          <Form.Group id="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" ref={emailref} required />
+          </Form.Group>
 
-         <Form.Group id = "password">
-         <Form.Label>Password</Form.Label>
-         <Form.Control type = "password" ref = {passowrdref} required />
-         </Form.Group>
+          <Form.Group id="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" ref={passwordRef} required />
+          </Form.Group>
 
+          <Form.Group id="confirm-password">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="email" ref={passwordConfirmRef} required />
+          </Form.Group>
 
-         <Form.Group id = "confirm-password" >
-         <Form.Label>Confirm Password</Form.Label>
-         <Form.Control type = "email" ref = {passwordConfirmref} required />
-         </Form.Group>
-        
-       
-        <Button type = "submit"> Sign Up </Button>
-        </Form.Group>
-        </Form.Label>
-
-    </Card.Body>
-    <Card.Body>
-      <div className="w-100 text-center mt-2">
-        Already have and account ? Log In
-      </div>
-    </Card.Body>
-  </h2>;
+          <Button type="submit"> Sign Up </Button>
+        </Form>
+      </Card.Body>
+      <Card.Body>
+        <div className="w-100 text-center mt-2">
+          Already have and account ? Log In
+        </div>
+      </Card.Body>
+    </>
+  );
 };
 
 export default Signup;
