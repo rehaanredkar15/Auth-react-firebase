d5f3cbff17aebefacb8abeeaa4f202082cd0d355
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-05-01 13:34:07 +0530

    fix - fixed a bug in logout
    used a private route for the dashboard

--- auth-react/src/components/PrivateRoute.js
+++ auth-react/src/components/PrivateRoute.js
@@ -1,6 +1,19 @@
 import React from "react";
+import {Route,Redirect} from 'react-router-dom';
+import {useAuth } from '../Contexts/AuthContext';
+//this is a wrapper for our  cureent route
+export default function PrivateRoute({component: Component,...rest}) {
+  return(
 
-export default function PrivateRoute() {
-  return;
-  <div></div>;
+  <Route
+    {...rest}>
+     render = {props => {
+
+         currentUser ? <Component { ...props} /> : <Redirect to ="/login"
+
+     }}
+  </Route>;
+
+  )
 }
+ 
