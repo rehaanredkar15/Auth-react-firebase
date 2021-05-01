69605987e9e1328a49b48848af7f0c29f004cdb8
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-05-01 13:38:11 +0530

    docs  - added the comments for better explanation

--- auth-react/src/components/PrivateRoute.js
+++ auth-react/src/components/PrivateRoute.js
@@ -3,12 +3,15 @@ import {Route,Redirect} from 'react-router-dom';
 import {useAuth } from '../Contexts/AuthContext';
 //this is a wrapper for our  cureent route
 export default function PrivateRoute({component: Component,...rest}) {
+  
+  const { currentUser} = useAuth()
+  
   return(
-
+    //cap
   <Route
     {...rest}>
      render = {props => {
-
+    //we will render the component which gets passed when we have currentuser  , if we dont do this then after the logout the dashbaord will render so we arranged it will now  
          currentUser ? <Component { ...props} /> : <Redirect to ="/login"
 
      }}
