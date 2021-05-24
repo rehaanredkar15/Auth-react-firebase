594c7a3249bb69f9244e75749a5edc894139a9e7
rehaan redkar <56068173+rehaanredkar15@users.noreply.github.com>
2021-05-25 01:58:07 +0530

    test- testing final css

--- auth-react/src/components/Googledrive/dashboard.js
+++ auth-react/src/components/Googledrive/dashboard.js
@@ -8,7 +8,7 @@ import Folder from "./Folder";
 import File from "./File";
 import { useParams, useLocation } from "react-router-dom";
 import FolderBreadcrumbs from "./FolderBreadcrumbs";
-
+import "../../App.css";
 export default function Dashboard() {
   const { folderId } = useParams();
   const { state = {} } = useLocation();
@@ -21,57 +21,57 @@ export default function Dashboard() {
   console.log(childFiles);
   return (
     <>
-      <div>
-        <Navbar />
-        <Container fluid>
-          <div className="d-flex align-items-center">
-            <FolderBreadcrumbs currentFolder={folder} />
-            <AddFileButton currentFolder={folder} />
-            <AddFolderButton currentFolder={folder} />
-          </div>
-
-          <Jumbotron>
-            <h5>Your Folders </h5>
-            {childFolders.length > 0 && (
-              <div className="d-flex flex-wrap">
-                {childFolders.map((childFolder) => (
-                  <div
-                    key={childFolder.id}
-                    style={{ maxWidth: "250px" }}
-                    className="p-2"
-                  >
-                    <Folder folder={childFolder} />
-                  </div>
-                ))}
-              </div>
-            )}
-
-            {childFolders.length > 0 && childFiles.length > 0 && <hr />}
-
-            <h5>Your Files </h5>
-            {childFiles.length > 0 && (
-              <div className="d-flex flex-wrap">
-                {childFiles.map((childFile) => (
-                  <div
-                    key={childFile.id}
-                    style={{ maxWidth: "250px" }}
-                    className="p-2"
-                  >
-                    <File file={childFile} />
-                  </div>
-                ))}
-              </div>
-            )}
-            <div className="text-center">
-              <h4>Hello, secret Keeper!</h4>
-              <p>
-                This is your select box , All of your private documents are Safe
-                here
-              </p>
+      <Navbar />
+      <Container fluid>
+        <div className="d-flex align-items-center">
+          <FolderBreadcrumbs currentFolder={folder} />
+          <AddFileButton currentFolder={folder} />
+          <AddFolderButton currentFolder={folder} />
+        </div>
+        {/* <div id="intro" class="view"> */}
+        <Jumbotron>
+          <h5>Your Folders </h5>
+          {childFolders.length > 0 && (
+            <div className="d-flex flex-wrap">
+              {childFolders.map((childFolder) => (
+                <div
+                  key={childFolder.id}
+                  style={{ maxWidth: "250px" }}
+                  className="p-2"
+                >
+                  <Folder folder={childFolder} />
+                </div>
+              ))}
             </div>
-          </Jumbotron>
-        </Container>
-      </div>
+          )}
+
+          {childFolders.length > 0 && childFiles.length > 0 && <hr />}
+
+          <h5>Your Files </h5>
+          {childFiles.length > 0 && (
+            <div className="d-flex flex-wrap">
+              {childFiles.map((childFile) => (
+                <div
+                  key={childFile.id}
+                  style={{ maxWidth: "250px" }}
+                  className="p-2"
+                >
+                  <File file={childFile} />
+                </div>
+              ))}
+            </div>
+          )}
+          <div className="text-center">
+            <h4>Hello, secret Keeper!</h4>
+            <p>
+              This is your select box , All of your private documents are Safe
+              here
+            </p>
+          </div>
+        </Jumbotron>
+
+        {/* </div> */}
+      </Container>
     </>
   );
 }
